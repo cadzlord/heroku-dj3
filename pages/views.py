15398e8e54d61bc93c0d0e2946400e27dfaa49c0
cadzlord <muhammadirsyad.anilo@gmail.com>
2018-02-12 16:26:10 +0700

    update perdana heroku django

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-
-def homePageView(request):
+def HelloPageView(request):
     return HttpResponse('Hello, World!')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class ShowTableView(TemplateView):
+	template_name = 'tableView.html'
+
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
 
 # Create your views here.
